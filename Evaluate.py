2bd86fdf866ebf0499482da2bcf2b6e12de06676
AboutDaxin <765974764@qq.com>
2024-04-06 18:09:22 +0800

    修改复杂度简化参数

--- Evaluate.py
+++ Evaluate.py
@@ -193,7 +193,7 @@ def evaluate(individual, problems_origin, test_index):
                 prcs_time_last = prcs_time_now
 
         individual.fitnesses.append(-makespan -
-                                    ((makespan*0.01*individual.tree_complexity()) if (test_index in [2, 3, 6, 7]) else 0))
+                                    ((makespan*0.01*individual.tree_complexity()) if (test_index in [0, 2, 4]) else 0))
         # 记录个体对本问题的优化目标值（不考虑其他策略影响，当前版本与适应度一致）
         individual.objectives.append(-makespan)
         # 添加各项目标函数值
