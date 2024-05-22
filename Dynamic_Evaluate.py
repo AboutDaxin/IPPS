b069c4970fbf53c43074eddfe05e4fff330e79dd
AboutDaxin <765974764@qq.com>
2024-05-22 14:55:18 +0800

    初成3

--- Dynamic_Evaluate.py
+++ Dynamic_Evaluate.py
@@ -4,6 +4,7 @@ from Modeling import Job
 
 # 实例化方法——适应度评估（包括两次试验不同的方法）
 def evaluate(individual, problems_origin, test_index, generation, solve_task):
+    global task
     # 复制源问题
     problems = copy.deepcopy(problems_origin)
     # 第几个算法?
@@ -72,76 +73,80 @@ def evaluate(individual, problems_origin, test_index, generation, solve_task):
                         station.queue.append(Job(station_task, station, true_time))
 
             # 之后，遍历所有可用task，进行job分配
-            stations_best = []
-            for task in available_tasks:
-                # 初始化一个备选station临时存放点
-                stations_temp = []
-                # 基于该task遍历所有station，释放一个job至对应station的job序列
-                for station in stations:
-                    # 如果该task的最前道序可以使用该station
-                    if task.process_path[0] in station.capability:
-                        # 生成备选station列表
-                        stations_temp.append(station)
-                        # 评估确定该station的优先级
-                        # 如果该station序列中存在job
-                        if station.queue:
-                            # 过渡优先值初始化
-                            priority_temp = 0
-                            # 遍历station中所有job并分别计算优先值
-                            for job in station.queue:
-                                # 临时优先值为所有遍历完job的优先值总和
-                                priority_temp += individual.root.left.interpret(job, station, true_time)
-                            # 得到该station的当前优先值
-                            station.priority = priority_temp
-                        # 如果该station序列中没有任务，则优先值跟num相关
-                        else:
-                            station.priority = station.station_index/100
-                # 确定被选中的station（优先值最小为最高级别）
-                stations_best.append(min(stations_temp)) if stations_temp else print("no!")
-            # 在job序列对应的station中加入一个Job
-            temp_task_index3 = [i for i, x in enumerate(stations_best) if x == min(stations_best)]
-            final_task = available_tasks[temp_task_index3[0]]
-            station_best = min(stations_best)
-            print("建议分配任务{0}的工序{1}至单元{2}。".format(final_task.task_index, final_task.process_string_index, station_best.station_index))
-            station_best.queue.append(Job(final_task, station_best, true_time))
-            # 该station排序状态改为“需要重排”
-            station_best.need_popped = False
-            # 该task状态变为“不需要重排”
-            final_task.need_popped = False
+            if available_tasks:
+                stations_best = []
+                for task in available_tasks:
+                    # 初始化一个备选station临时存放点
+                    stations_temp = []
+                    # 基于该task遍历所有station，释放一个job至对应station的job序列
+                    for station in stations:
+                        # 如果该task的最前道序可以使用该station
+                        if task.process_path[0] in station.capability:
+                            # 生成备选station列表
+                            stations_temp.append(station)
+                            # 评估确定该station的优先级
+                            # 如果该station序列中存在job
+                            if station.queue:
+                                # 过渡优先值初始化
+                                priority_temp = 0
+                                # 遍历station中所有job并分别计算优先值
+                                for job in station.queue:
+                                    # 临时优先值为所有遍历完job的优先值总和
+                                    priority_temp += individual.root.left.interpret(job, station, true_time)
+                                # 得到该station的当前优先值
+                                station.priority = priority_temp
+                            # 如果该station序列中没有任务，则优先值跟num相关
+                            else:
+                                station.priority = station.station_index/100
+                    # 确定被选中的station（优先值最小为最高级别）
+                    stations_best.append(min(stations_temp)) if stations_temp else print("no!")
+                # 在job序列对应的station中加入一个Job
+                temp_task_index3 = [i for i, x in enumerate(stations_best) if x == min(stations_best)]
+                final_task = available_tasks[temp_task_index3[0]]
+                station_best = min(stations_best)
+                print("建议分配任务{0}的工序{1}至单元{2}。".format(final_task.task_index, final_task.process_string_index, station_best.station_index))
+                station_best.queue.append(Job(final_task, station_best, true_time))
+                # 该station排序状态改为“需要重排”
+                station_best.need_popped = False
+                # 该task状态变为“不需要重排”
+                final_task.need_popped = False
 
-            # # 排序规则
-            station = station_best
-            # 对该station的job序列执行遍历，重排
-            for job in station.queue:
-                # 计算该job的优先级数值
-                job.priority = individual.root.right.interpret(job, station, true_time)
-            # 随机排列该station的job序列
-            # shuffle(station.queue)
-            # 将该station的job序列按优先级从小到大排序（根据Job的富比较方法）
-            station.queue.sort()
-            print("建议单元{0}上的任务排序如下：".format(station.station_index))
-            for i in range(len(station.queue)):
-                print(str(i+1) + ":任务{}的工序{}。".format(station.queue[i].task.task_index, station.queue[i].task.task_string_index))
-            # 对station进行能力变更
-            if station.current_capability == 0:
-                # 初始情况
-                station.current_capability = station.queue[0].task.process_path[0] if station.queue else 0
-                station.have_trans = True
-                station.current_trans_time = station.configuration_time
-                total_transtime += station.configuration_time
-                print("单元能力需要转换，消耗{0}小时。".format(station.configuration_time))
-            elif station.current_capability == station.queue[0].task.process_path[0] if station.queue else 0:
-                # 不需要转变
-                station.have_trans = False
-                station.current_trans_time = 0
-                print("单元能力不需要转换。")
+                # # 排序规则
+                station = station_best
+                # 对该station的job序列执行遍历，重排
+                for job in station.queue:
+                    # 计算该job的优先级数值
+                    job.priority = individual.root.right.interpret(job, station, true_time)
+                # 随机排列该station的job序列
+                # shuffle(station.queue)
+                # 将该station的job序列按优先级从小到大排序（根据Job的富比较方法）
+                station.queue.sort()
+                print("建议单元{0}上的任务排序如下：".format(station.station_index))
+                for i in range(len(station.queue)):
+                    print(str(i+1) + ":任务{}的工序{}。".format(station.queue[i].task.task_index, station.queue[i].task.task_string_index))
+                # 对station进行能力变更
+                if station.current_capability == 0:
+                    # 初始情况
+                    station.current_capability = station.queue[0].task.process_path[0] if station.queue else 0
+                    station.have_trans = True
+                    station.current_trans_time = station.configuration_time
+                    total_transtime += station.configuration_time
+                    print("单元能力需要转换，消耗{0}小时。".format(station.configuration_time))
+                elif station.current_capability == station.queue[0].task.process_path[0] if station.queue else 0:
+                    # 不需要转变
+                    station.have_trans = False
+                    station.current_trans_time = 0
+                    print("单元能力不需要转换。")
+                else:
+                    # 需要转变
+                    station.current_capability = station.queue[0].task.process_path[0] if station.queue else 0
+                    station.have_trans = True
+                    station.current_trans_time = station.configuration_time
+                    total_transtime += station.configuration_time
+                    print("单元能力需要转换，消耗{0}小时。".format(station.configuration_time))
+                # 排序状态变为已排完，不需要重排
+                station.have_popped = True
+                have_finished = True
             else:
-                # 需要转变
-                station.current_capability = station.queue[0].task.process_path[0] if station.queue else 0
-                station.have_trans = True
-                station.current_trans_time = station.configuration_time
-                total_transtime += station.configuration_time
-                print("单元能力需要转换，消耗{0}小时。".format(station.configuration_time))
-            # 排序状态变为已排完，不需要重排
-            station.have_popped = True
-            have_finished = True
+                print("任务{}已完成所有工序。".format(task.task_index))
+                have_finished = True
--- mainDynamic.py
+++ mainDynamic.py
@@ -1,5 +1,5 @@
 from Dynamic_instance import Dynamic_instance
 
 # 需要进行实时调度的任务号
-task_index = 9
+task_index = 10
 Dynamic_instance(task_index)
